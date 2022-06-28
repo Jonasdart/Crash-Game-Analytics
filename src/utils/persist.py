0a449c887ad94d4f6fec53cf09db641b2ae46da1
jonasdart <duarte.jsystem@gmail.com>
2022-06-28 20:28:01 -0300

    feat: Removed data treatment of collect escope

--- src/utils/persist.py
+++ src/utils/persist.py
@@ -7,12 +7,11 @@ def save_ship_data(ship, bets=[]) -> bool:
     Ship = Query()
 
     if not table.get(Ship._id == ship["_id"]):
-        ship["win"] = int(ship["result"] >= 2)
         ship["bets"] = bets
 
         inserted = table.insert(ship) > 0
         print(f"---- Multiplicador: {ship['result']} -  {len(bets)} Apostas nesta rodada ----")
-        
+
         return inserted
     
     return False
